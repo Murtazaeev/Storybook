d83e95fc7586c43d64c003e64428ecc1e1af7b13
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2021-10-03 00:04:42 +0200

    A comment added to Button.stories.js file

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -10,3 +10,4 @@ export const Primary = () => <Button variant='primary'>Primary</Button>
 export const Secondary = () => <Button variant='secondary'>Secondary</Button>
 export const Succes = () => <Button variant='succes'>Succes</Button>
 export const Danger = () => <Button variant='denger'>Danger</Button>
+// this is a comment
